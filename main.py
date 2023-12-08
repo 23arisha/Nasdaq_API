e76c58272f8cd1fc195c53c59a9eec849b58aebf
Areesha_Farid <111558544+23arisha@users.noreply.github.com>
2023-12-08 21:32:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from flask import Flask, jsonify
-import apinasdata
-
-app = Flask(__name__)
-
-@app.route('/scrape_nasdaq')
-def scrape_nasdaq_route():
-    try:
-        # Call the scrape_nasdaq function
-        apinasdata.scrape_nasdaq()
-
-        # Provide a success response
-        return jsonify({'status': 'success', 'message': 'Scraping completed successfully'})
-    except Exception as e:
-        # Provide an error response if an exception occurs
-        return jsonify({'status': 'error', 'message': str(e)})
+import streamlit as st
+import apinasdata  # Import your scraping function
 
 def main():
-    app.run(host='127.0.0.1', debug=True, port=5000, threaded=True)
+    st.title('Nasdaq Scraping App')
+
+    # Button to trigger scraping
+    if st.button('Scrape Nasdaq Data'):
+        try:
+            # Call the scrape_nasdaq function
+            apinasdata.scrape_nasdaq()
+
+            # Display success message
+            st.success('Scraping completed successfully')
+        except Exception as e:
+            # Display error message if an exception occurs
+            st.error(f'Error: {str(e)}')
 
 if __name__ == '__main__':
     main()
